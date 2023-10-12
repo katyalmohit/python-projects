afec340ffb01902e8d5c16114d399576acbe56e3
Mohit <128162892+katyalmohit@users.noreply.github.com>
2023-10-12 23:15:06 +0530

    Generate the marker

--- image-processing/aruco/basics.py
+++ image-processing/aruco/basics.py
@@ -2,5 +2,9 @@ import cv2 as cv
 import cv2.aruco as aruco
 import numpy as np
 
-#Load the predefined dictionaries
+# Load the predefined dictionaries
 dictionary = aruco.Dictionary_get(aruco.DICT_6X6_250)
+
+# Generate the marker
+markerImage = np.zeros((250,250), dtype=np.uint8)
+markerImage = aruco.drawMarker(dictionary, 33, 200, markerImage, 1)
